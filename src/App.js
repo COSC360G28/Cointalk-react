ccd44ac25e38de791f5af52294f39156bc9fa33c
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-02-17 10:13:14 -0800

    CategorySelector init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { NavBar } from "./components/navBar/NavBar";
+import { CategorySelector } from "./components/categorySelector/CategorySelector";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <CategorySelector />
     </div>
   );
 }
---
+++ src/components/categorySelector/CategorySelector.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./styles.scss";
+
+export const CategorySelector = () => {
+  return <div id="category-selector"></div>;
+};
