c4c73565a42d90bd5b184d03d28a12c01932a3ae
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-02-17 15:56:20 -0800

    Add login button

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "./styles.scss";
+import { ReactComponent as SignIn } from "../../assets/box-arrow-in-right.svg";
 
 export const NavBar = () => {
   return (
     <nav>
       <h1 href="/">COINTALK</h1>
+      <div className="login-desktop">
+        <a href="/login" className="sign-in">
+          Sign In
+        </a>
+        <a href="/login" className="sign-up">
+          <h3>Sign Up</h3>
+        </a>
+      </div>
+      <a href="/login" className="login-mobile">
+        <SignIn />
+      </a>
     </nav>
   );
 };
