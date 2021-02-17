bd3d882101f42dbe148d5940101e0fe233791672
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-02-17 11:16:31 -0800

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import "./App.css";
 import { NavBar } from "./components/navBar/NavBar";
 import { MainPage } from "./pages/mainPage/MainPage";
+import { Login } from "./pages/login/Login";
+import { Post } from "./pages/post/Post";
+import { User } from "./pages/user/User";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <MainPage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact>
+            <NavBar />
+            <MainPage />
+          </Route>
+          <Route path="/login" exact>
+            <Login />
+          </Route>
+          <Route path="/post" exact>
+            <Post />
+          </Route>
+          <Route path="/user" exact>
+            <User />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
