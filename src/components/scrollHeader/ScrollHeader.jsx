a91904850325bb30e8da949b2b4c26fb6978006a
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-02-17 14:30:41 -0800

    Added Icons to header items

--- src/components/scrollHeader/ScrollHeader.jsx
+++ src/components/scrollHeader/ScrollHeader.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import "./styles.scss";
+import { ReactComponent as Search } from "../../assets/search.svg";
+import { ReactComponent as Filter } from "../../assets/filter-left.svg";
 
 export const ScrollHeader = () => {
   return (
     <div id="scroll-header">
       <div className="content">
-        <input type="text" placeholder="SEARCH" />
-        <select name="sort">
-          <option value="hot">Hot</option>
-          <option value="new">New</option>
-        </select>
+        <div className="wrapper">
+          <Search />
+          <input type="text" placeholder="SEARCH" />
+        </div>
+        <div className="wrapper">
+          <select name="sort" dir="rtl">
+            <option value="hot">HOT</option>
+            <option value="new">NEW</option>
+          </select>
+          <Filter />
+        </div>
       </div>
     </div>
   );
