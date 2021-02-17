6f35ffe19e247a3955f10293502804d13f2bf22b
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-02-17 14:39:49 -0800

    Set state on input change

--- src/components/scrollHeader/ScrollHeader.jsx
+++ src/components/scrollHeader/ScrollHeader.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 import { ReactComponent as Search } from "../../assets/search.svg";
 import { ReactComponent as Filter } from "../../assets/filter-left.svg";
 
 export const ScrollHeader = () => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <div id="scroll-header">
       <div className="content">
         <div className="wrapper">
           <Search />
-          <input type="text" placeholder="SEARCH" />
+          <input type="text" placeholder="SEARCH" onChange={handleChange} />
         </div>
         <div className="wrapper">
           <select name="sort" dir="rtl">
