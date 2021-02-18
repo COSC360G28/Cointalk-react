7bd783562d627c790afdb9d611342270a8cba6e6
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-02-18 15:15:23 -0800

    Setup Post Page

--- src/pages/post/Post.jsx
+++ src/pages/post/Post.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { NavBar } from "../../components/navBar/NavBar";
 import "./styles.scss";
+import { MainContent, Content } from "../../components/containers/Containers";
 
 export const Post = () => {
-    return (
-        <>
-            <NavBar />
-        </>
-    );
+  return (
+    <>
+      <NavBar />
+      <MainContent>
+        <Content>{/* <PostCard/> */}</Content>
+        <Content>{/* <NewComment/> */}</Content>
+        <Content>{/* <Comments/> */}</Content>
+      </MainContent>
+    </>
+  );
 };
