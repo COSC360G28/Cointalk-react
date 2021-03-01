c6edf48ce9f1502dd6eaaf576784cd526c791f8a
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-03-01 13:27:13 -0800

    Setup new comment component

--- react-app/src/components/newComment/NewComment.jsx
+++ react-app/src/components/newComment/NewComment.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
+import { ReactComponent as NewCommentIcon } from "../../assets/pencil-square.svg";
+import { ReactComponent as SendIcon } from "../../assets/arrow-right-circle.svg";
 
 export const NewComment = () => {
-    return <div/>
-}
+  const [text, setText] = useState();
+
+  const submit = () => {
+    // TODO: submit comment
+    console.log(text);
+  };
+
+  return (
+    <div id="new-comment-container">
+      <NewCommentIcon />
+      <input
+        placeholder="New comment..."
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
+      <button onClick={submit} className={text ? null : "disabled"}>
+        <SendIcon className="send" />
+      </button>
+    </div>
+  );
+};
