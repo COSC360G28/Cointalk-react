d7fca61b04fa44bf67d5486913eb17cd2a277564
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-03-06 12:14:14 -0800

    Swapped upload prompt image

--- react-app/src/pages/signUp/SignUp.jsx
+++ react-app/src/pages/signUp/SignUp.jsx
@@ -2,29 +2,37 @@ import React, { useState } from "react";
 import "./styles.scss";
 import { Button } from "../../components/button/Button";
 import { UserAccessForm } from "../../components/userAccessForm/UserAccessForm";
-import cameraIcon from "../../assets/iconCameraProfilePictureUpload.webp"
-
+import { ReactComponent as CameraIcon } from "../../assets/camera-fill.svg";
 
 export const SignUp = () => {
-
-  const [profilePicture, setProfilePicture] = useState(cameraIcon);
+  const [profilePicture, setProfilePicture] = useState();
 
   function handleProfilePictureUpload(event) {
-    if(event.target.files[0] != null) {
+    if (event.target.files[0] != null) {
       setProfilePicture(URL.createObjectURL(event.target.files[0]));
     }
   }
 
   return (
     <UserAccessForm>
-      <label for="profile-upload" class="custom-file-upload" >
-        <img src={profilePicture} />
+      <label htmlFor="profile-upload" className="custom-file-upload">
+        {profilePicture ? <img src={profilePicture} /> : <CameraIcon />}
       </label>
-      <input type="file" id="profile-upload" name="filename" onChange={handleProfilePictureUpload} accept="image/png, image/jpeg" />
+      <input
+        type="file"
+        id="profile-upload"
+        name="filename"
+        onChange={handleProfilePictureUpload}
+        accept="image/png, image/jpeg"
+      />
       <input type="text" name="username" placeholder="Username" />
       <input type="text" name="email" placeholder="Email" />
       <input type="text" name="password" placeholder="Password" />
-      <input type="text" name="confirmPassword" placeholder="Confirm Password" />
+      <input
+        type="text"
+        name="confirmPassword"
+        placeholder="Confirm Password"
+      />
       <Button text="Sign In" />
     </UserAccessForm>
   );
