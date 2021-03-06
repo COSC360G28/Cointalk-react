5b422cd6d0ffac5c1f65b4adc7f695573ba13484
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-03-06 12:54:42 -0800

    Setup test end-points

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,22 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('App is running');
+app.use(express.json());
+
+// Test to see that app is running
+app.get('/test', (req, res) => {
+    res.send('App is running...\n');
+});
+
+// Test to see what data is recieved from the application
+app.post('/test', (req, res) => {
+    // Create string to send back
+    let response: string = 'App is running...\n';
+    response += 'App recieved: \n';
+    response += JSON.stringify(req.body, null, 2);
+
+    // Return repsonse string to user
+    res.send(response);
 });
 
 // Listen for requests on port 5000
