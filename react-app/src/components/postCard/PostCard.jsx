968b238f8f0095670ed056d967b5a1620ececec4
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-03-08 11:59:11 -0800

    Setup post desktop

--- react-app/src/components/postCard/PostCard.jsx
+++ react-app/src/components/postCard/PostCard.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 import "./styles.scss";
 
-export const PostCard = () => {
-    return <div/>
-}
+export const PostCard = ({ post }) => {
+  return (
+    <div id="post">
+      <div className="post-title-container">
+        <h2>{post.title}</h2>
+        <img
+          className="profile-image"
+          alt="Profile"
+          src={post.user.profileImage}
+        />
+        <h3>{post.user.username}</h3>
+        <p className="stars">{post.stars}</p>
+      </div>
+      {post.image ? (
+        <div className="post-image-container">
+          <img alt="Post" src={post.image} />
+        </div>
+      ) : null}
+      <p>{post.text}</p>
+    </div>
+  );
+};
