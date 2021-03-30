fa5f436a4426770526341998a295df9f5af696a9
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-03-30 12:36:34 -0700

    Fixed DB connection

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,32 +1,43 @@
 import { Client } from 'pg';
+import fs from 'fs';
 
 export class Connection {
-
     private client: Client;
 
     constructor() {
         this.client = new Client({
             user: 'postgres',
-            host: 'localhost',
+            database: 'postgres',
+            host: 'postgres',
             password: 'pg_pass',
             port: 5432,
-            query_timeout: 10000
-        })
+            query_timeout: 10000,
+        });
         this.connect();
     }
 
     connect() {
-        this.client.connect(err => {
+        this.client.connect((err) => {
             if (err) {
                 console.error('connection error', err.stack);
             }
-        })
+        });
     }
     disconnect() {
-        this.client.end()
+        this.client.end();
     }
 
     getConnection() {
         return this.client;
+    }
+
+    populate() {
+        fs.readFile('Database.ddl', 'utf-8', (err, data) => {
+            if (err) throw err;
+            // console.log(data);
+            this.client.query(data, (err) => {
+                if (err) throw err;
+            });
+        });
     }
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,9 @@
 import { app } from './server';
+import { Connection } from './database';
+
+// Set Default db data
+const db = new Connection();
+db.populate();
 
 // Listen for requests on port 5000
 app.listen('5000');
