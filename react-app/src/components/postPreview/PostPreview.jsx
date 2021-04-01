bedd4dc2d67e07fd0a381391ce92c14e052f9ec5
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-03-31 17:56:41 -0700

    Link to post on PostPreview click

--- react-app/src/components/postPreview/PostPreview.jsx
+++ react-app/src/components/postPreview/PostPreview.jsx
@@ -3,16 +3,35 @@ import "./styles.scss";
 import { ReactComponent as UserIcon } from "../../assets/user.svg";
 import { Stars } from "../stars/Stars";
 
-export const PostPreview = ({ title, image, text, username, score, liked }) => {
+export const PostPreview = ({
+  pid,
+  title,
+  image,
+  text,
+  username,
+  score,
+  liked,
+}) => {
+  const goToPost = () => {
+    window.location.href = `/post/${pid}`;
+  };
+
   return (
     <div className="post-preview">
       {image ? (
-        <img className="post-preview-image" src={image} alt="Post" />
+        <img
+          onClick={goToPost}
+          className="post-preview-image"
+          src={image}
+          alt="Post"
+        />
       ) : null}
       <div className="post-preview-text-container">
         <div className="post-preview-header">
           <div className="post-preview-info-container">
-            <h3 className="post-preview-title">{title}</h3>
+            <h3 onClick={goToPost} className="post-preview-title">
+              {title}
+            </h3>
             <div className="user-container">
               <UserIcon className="user-icon" />
               <p>{username}</p>
