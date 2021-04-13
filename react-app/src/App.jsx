c4fea167c413f5fd78dcfa7c76aa12d42caa07ef
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-04-13 08:35:24 -0700

    Add provider

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,31 +1,35 @@
 import "./App.scss";
+import { createContext } from "react";
 import { Main, Login, Post, User, SignUp, Logout } from "./pages/";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { UserContext } from "./Contexts.js";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Switch>
-          <Route path="/" exact>
-            <Main />
-          </Route>
-          <Route path="/login" exact>
-            <Login />
-          </Route>
-          <Route path="/signup" exact>
-            <SignUp />
-          </Route>
-          <Route path="/post/:id" exact>
-            <Post />
-          </Route>
-          <Route path="/user" exact>
-            <User />
-          </Route>
-          <Route path="/logout" exact>
-            <Logout />
-          </Route>
-        </Switch>
+        <UserContext.Provider value={null}>
+          <Switch>
+            <Route path="/" exact>
+              <Main />
+            </Route>
+            <Route path="/login" exact>
+              <Login />
+            </Route>
+            <Route path="/signup" exact>
+              <SignUp />
+            </Route>
+            <Route path="/post/:id" exact>
+              <Post />
+            </Route>
+            <Route path="/user" exact>
+              <User />
+            </Route>
+            <Route path="/logout" exact>
+              <Logout />
+            </Route>
+          </Switch>
+        </UserContext.Provider>
       </div>
     </BrowserRouter>
   );
